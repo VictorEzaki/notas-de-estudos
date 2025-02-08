bcbec43c953addcf02d0cfd08d348ed1ddcd7ea9
Victor Ezaki <victorezaki19@gmail.com>
2025-02-08 18:48:41 -0300

    conteudos finais de typescript

--- api-typescript/src/server.ts
+++ api-typescript/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { router } from './routes';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json())
 app.use('/api', router)
+app.use(errorHandler)
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}`))
