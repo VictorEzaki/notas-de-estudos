744c9cb64cd223f82acab95e1211ec50f95941ea
Victor Ezaki <victorezaki19@gmail.com>
2025-02-11 16:35:20 -0300

    API completa com conexão com banco de dados utilizando o PG

--- sql-no-nodejs-e-prima-orm/api-com-banco-de-dados/src/routes.js
+++ sql-no-nodejs-e-prima-orm/api-com-banco-de-dados/src/routes.js
@@ -1,12 +1,25 @@
 const { Router } = require("express");
-const productsController = require('./controllers/products-controller')
+const productsController = require("./controllers/products-controller");
+const ordersController = require("./controllers/orders-controller");
+const customersController = require("./controllers/costumers-controller");
 
 const router = Router();
 
 router.get("/products", productsController.index);
-router.post("/products", productsController.create);
 router.get("/products/:id", productsController.show);
+router.post("/products", productsController.create);
 router.put("/products/:id", productsController.update);
 router.delete("/products/:id", productsController.delete);
+
+router.get("/customers", customersController.index);
+router.get("/customers/:id", customersController.show);
+router.post("/customers", customersController.create);
+router.put("/customers/:id", customersController.update);
+router.delete("/customers/:id", customersController.delete);
+
+router.get("/orders", ordersController.index);
+router.get("/orders/:id", ordersController.show);
+router.post("/orders", ordersController.create);
+router.delete("/orders/:id", ordersController.delete);
 
 module.exports = router;
