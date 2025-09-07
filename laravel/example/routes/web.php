1deb5237c811b35ebb93ee8932037d0cff417e3b
Victor Ezaki <victorezaki19@gmail.com>
2025-09-07 18:28:06 -0300

    conteudos iniciais de laravel

---
+++ laravel/example/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
