af549972a74c835e232524b17575c574d85ab47f
Victor Ezaki <victorezaki19@gmail.com>
2025-09-14 16:44:04 -0300

    eloquent

--- laravel/example/routes/web.php
+++ laravel/example/routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
