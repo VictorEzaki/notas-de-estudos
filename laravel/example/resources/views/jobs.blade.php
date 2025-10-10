5179bf17e4593a29c432280508ac5e0628e20214
Victor Ezaki <victorezaki19@gmail.com>
2025-10-10 14:00:42 -0300

    conteudo sobre paginação

--- laravel/example/resources/views/jobs.blade.php
+++ laravel/example/resources/views/jobs.blade.php
@@ -10,7 +10,7 @@
         </form> --}}
 
         @foreach ($jobs as $job)
-            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded">
                 <div class="font-bold text-blue-500">
                     {{ $job->employer->name }}
                 </div>
