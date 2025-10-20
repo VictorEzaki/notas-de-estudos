c19c7b2b8bd4422ef96d483fb77757fd10639017
Victor Ezaki <victorezaki19@gmail.com>
2025-10-19 22:33:41 -0300

    mais conteudo

--- laravel/example/routes/web.php
+++ laravel/example/routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(10);
 
@@ -15,28 +16,71 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required']
     ]);
-
+    
     Job::create([
         'employer_id' => 1,
         'title' => request('title'),
         'salary' => request('salary')
     ]);
+    
+    return redirect('/jobs');
+});
 
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::findOrFail($id);
+    
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (On hold...)
+
+    // update the job
+    // and persist
+    $job = Job::findOrFail($id);
+    
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold...)
+    // delete the job
+    Job::findOrFail($id)->delete();
+
+    // redirect
     return redirect('/jobs');
 });
 
