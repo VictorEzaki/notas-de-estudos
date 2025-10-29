6c3e3ff9c2e62903246a179e264024ebfbfa0a86
victor@soft4 <victor@soft4.com.br>
2025-10-29 14:09:20 -0300

    gates, middleware e policies

--- laravel/example/routes/web.php
+++ laravel/example/routes/web.php
@@ -13,37 +13,40 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-// Route::controller(JobController::class)->group(function () {
-//     // Index
-//     Route::get('/jobs', 'index');
+Route::controller(JobController::class)->group(function () {
+    // Index
+    Route::get('/jobs', 'index');
 
-//     // Create
-//     Route::get('/jobs/create', 'create');
+    // Create
+    Route::get('/jobs/create', 'create');
 
-//     // Show
-//     Route::get('/jobs/{job}', 'show');
+    // Show
+    Route::get('/jobs/{job}', 'show');
 
-//     // Store
-//     Route::post('/jobs', 'store');
+    // Store
+    Route::post('/jobs', 'store')->middleware('auth');
 
-//     // Edit
-//     Route::get('/jobs/{job}/edit', 'edit');
+    // Edit
+    Route::get('/jobs/{job}/edit', 'edit')
+        ->middleware(['auth'])
+        ->can('edit', 'job')
+    ;
 
-//     // Update
-//     Route::patch('/jobs/{job}', 'update');
+    // Update
+    Route::patch('/jobs/{job}', 'update');
 
-//     // Destroy
-//     Route::delete('/jobs/{job}', 'destroy');
-// });
+    // Destroy
+    Route::delete('/jobs/{job}', 'destroy');
+});
 
 // Mesma coisa que
-Route::resource('jobs', JobController::class);
+// Route::resource('jobs', JobController::class);
 
 // Rotas de autenticação
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionUserController::class, 'create']);
+Route::get('/login', [SessionUserController::class, 'create'])->name('login');
 Route::post('/login', [SessionUserController::class, 'store']);
 Route::post('/logout', [SessionUserController::class, 'destroy']);
 
